766a312c29c57873ec4c9a4d38e522807a37fad9
Mazeda-Shathi <mazedaaktershathi2019@gmail.com>
2021-11-04 00:49:16 +0600

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb')
-const app = express()
-const port = 5000
+const app = express();
+const port = process.env.PORT || 5000;
 const ObjectId = require('mongodb').ObjectId
 const cors = require('cors')
 require('dotenv').config()
